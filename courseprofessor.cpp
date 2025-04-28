ade83d9d6c9c29296f4afd30a13121221c7b5c03
rozhina <sheikholeslamr@gmail.com>
2025-04-28 20:11:24 +0330

    courseprofessor.cpp final

--- courseprofessor.cpp
+++ courseprofessor.cpp
@@ -4,3 +4,7 @@ void CourseProfessor::createClass(const Course& course) {
     classrooms.emplace_back(course.getCourseName());
     std::cout << "Class for " << course.getCourseName() << " created." << std::endl;  
 }  
+
+const std::vector<Classroom>& CourseProfessor::getClassrooms() const {  
+    return classrooms;  
+}  
