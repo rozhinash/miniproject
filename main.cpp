5e3861a90bc7999b935893549159e8fa5c853665
rozhina <sheikholeslamr@gmail.com>
2025-04-28 20:33:43 +0330

    main.cpp login

--- main.cpp
+++ main.cpp
@@ -37,4 +37,58 @@ int main() {
         }  
     }  
 
+    if (!loggedIn) {  
+        cout << "Invalid username or password." << endl;  
+    }  
+  
+    ofstream outFile("yekestan.txt");  
+    if (outFile.is_open()) {  
+        for (const auto& user : users) {  
+            user->saveToFile(outFile);  
+        }  
+        for (const auto& classroom : classrooms) {  
+            classroom->saveToFile(outFile);  
+        }  
+        outFile.close();  
+    } else {  
+        cout << "Error opening file!" << endl;  
+    }                                                         
+     if (userType == 1) {
+            Student *student = dynamic_cast<Student *>(users.back()); 
+            int choice;  
+            do {  
+                cout << "1. List Enrolled Classes" << endl;  
+                cout << "2. Show Available Courses" << endl;  
+                cout << "3. Enroll in a Course" << endl;  
+                cout << "4. Exit" << endl;  
+                cout << "Enter your choice: ";  
+                cin >> choice;  
+
+                switch (choice) {  
+                    case 1:  
+                        student->listEnrolledClasses();  
+                        break;  
+                    case 2:  
+                        student->showAvailableCourses(classrooms);
+                        break;  
+                    case 3:   
+                        int courseIndex;  
+                        cout << "Choose a course to enroll (0 for Math, 1 for Science): ";  
+                        cin >> courseIndex; 
+                        if (courseIndex >= 0 && courseIndex < classrooms.size()) {  
+                            student->enrollInClass(*classrooms[courseIndex]);  
+                        } else {  
+                            cout << "Invalid course selection!" << endl;  
+                        }  
+                        break;  
+                    case 4:  
+                        cout << "Exiting..." << endl;  
+                        break;  
+                    default:  
+                        cout << "Invalid choice. Please try again." << endl;  
+                        break;  
+                }  
+            } while (choice != 4);  
+        } 
+
 }
