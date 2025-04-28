e276184befdff7cd1e59fed296512edf21ccdd57
rozhina <sheikholeslamr@gmail.com>
2025-04-28 19:29:07 +0330

    class classroom

---
+++ classroom.cpp
@@ -0,0 +1,26 @@
+#include "Classroom.h"  
+#include  <fstream>
+
+Classroom::Classroom(const Course& c) : course(c) {}  
+
+void Classroom::enrollStudent(int studentID) {  
+    if (EnrolledStudents.size() < course.getCapacity()) {  
+        EnrolledStudents.push_back(studentID);  
+        cout << "Student with ID " << studentID << " has been enrolled in " << course.getCourseName() << "." << endl;  
+    }  
+    else {  
+        cout << "Class is full. Cannot enroll student." << endl;  
+    }  
+}  
+
+void Classroom::listEnrolledStudents() const {  
+    cout << "Enrolled students in " << course.getCourseName() << ":" << endl;  
+    for (int id : EnrolledStudents) {  
+        cout << "Student ID: " << id << endl;  
+    }  
+}  
+
+int Classroom::getEnrolledStudentsCount() const {  
+    return EnrolledStudents.size();  
+} 
+
